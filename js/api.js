2d571e3d15d6483fe82348c28c2ad6684700a229
Al3J077 <garciaalejo711@gmail.com>
2025-04-23 03:27:27 -0500

    errorfavorito

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@ async function guardarFavoritoPorId(id) {
   try {
     const response = await fetch(`https://api.tvmaze.com/shows/${id}`);
     const serie = await response.json();
-    guardarFavorito(serie);
+    guardarFavorito(serie); // función que ya está en favoritos.js
   } catch (error) {
     console.error("Error al guardar favorito:", error);
     alert("No se pudo guardar la serie en favoritos.");
