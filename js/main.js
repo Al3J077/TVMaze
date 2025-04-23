afccdd3710a7022a3767288977393e27712213b5
Al3J077 <garciaalejo711@gmail.com>
2025-04-23 02:51:27 -0500

    sera?

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   navigate("home");
 
+});
   
   document.addEventListener("click", (e) => {
     if (e.target && e.target.id === "btn-favoritos") {
       renderFavoritos();
     }
   });
-});
+
 
